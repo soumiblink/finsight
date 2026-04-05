cfa309506cd14f416454d742dca31a0a048ccd0c
soumiblink <soumiblink@gmail.com>
2026-04-05 14:39:58 +0530

    update

--- manage.py
+++ manage.py
@@ -1,10 +1,9 @@
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finsight_core.settings')
     try:
         from django.core.management import execute_from_command_line
